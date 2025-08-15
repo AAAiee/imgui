9c20fb24b98b4dc4bd6ff95ff2d40f7cd126d2a6
Lex Yuan <yhl20010504@gmail.com>
2025-08-15 01:31:42 -0700

    Added dependencies (glfw and opengl)

--- Build-imGui.lua
+++ Build-imGui.lua
@@ -7,10 +7,21 @@ project "ImGui"
 	targetdir ("../../../Binaries/" .. OutputDir .. "/%{prj.name}")
 	objdir ("../../../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
+
+	includedirs{
+		".",
+		"../glfw/include"
+	}
+
+	links{
+		"GLFW",
+		"opengl32.lib"
+	}
+
     files
 	{
 		"imconfig.h",
-		"imgui.h",
+		"imgui.h", 
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
@@ -19,7 +30,9 @@ project "ImGui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.cpp"
 	}
 
 	filter "system:windows"
